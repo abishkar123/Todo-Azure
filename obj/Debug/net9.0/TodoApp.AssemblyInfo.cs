ce74a061a52c5df2e51efa120782517e074e9b43
abishkar rai <raiabishkar0.5@gmail.com>
2025-12-23 20:50:32 +1100

    dev enviroment setup

--- obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+997a6aed5e81bd5ec6f50cc1aa4aade15848e3b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0904c2a305fb467b08ada55dbf8e27272c263637")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
